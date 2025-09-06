795ddf6cf723c02a487078e88e45aa10fc2a5677
lucasrodrigues2007 <157885663+lucasrodrigues2007@users.noreply.github.com>
2025-09-06 11:41:12 -0300

    Add files via upload

---
+++ aula11/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+    // Isto parece HTML, mas é JSX!
+    return (
+      <div className="container">
+        <h1>Olá, Mundo com React e JSX!</h1>
+        <p>Meu primeiro componente React.</p>
+      </div>
+    );
+    // Nota: usamos className em vez de class para CSS.
+  }
+
+  
+
